935419280dc7a7e04041bb2813b9650b3df82c7c
hdm007 <harry.mardirossian13@gmail.com>
2021-02-12 14:49:20 -0500

    fixed hooks and changed custom names back to default, need to implement tests and label spans

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -14,7 +14,6 @@ export default class PlantList extends Component {
     axios
     .get("http://localhost:3333/plants")
     .then((res) => {
-      //console.log(res.data)
       this.setState({
         plants: res.data.plantsData,
       });
